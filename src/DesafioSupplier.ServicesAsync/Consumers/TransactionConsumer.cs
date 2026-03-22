ac32fc6a1e40efa8b9df61acda2ad7b0551aaffa
Stenio Nobres Batista <stenio.nobres@gmail.com>
2026-03-22 16:33:36 -0300

    feat: atualiza limite do Cliente a partir da mensagem recebida no Consumer

--- src/DesafioSupplier.ServicesAsync/Consumers/TransactionConsumer.cs
+++ src/DesafioSupplier.ServicesAsync/Consumers/TransactionConsumer.cs
@@ -1,18 +1,49 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using DesafioSupplier.Domain.Entities;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DesafioSupplier.Domain.Interfaces.Services;
 using DesafioSupplier.ServicesAsync.Configuration;
 
 namespace DesafioSupplier.ServicesAsync.Consumers;
 
 public class TransactionConsumer : ConsumerBaseRabbitMQ
 {
-    public TransactionConsumer(IConfiguration configuration, IOptions<ServerSettingsRabbitMQ> rabbitServerSettings) 
-                        : base(configuration["TransactionSettings:QueueName"] ?? string.Empty, rabbitServerSettings)
+    private readonly ILogger<TransactionConsumer> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public TransactionConsumer(IConfiguration configuration,
+                               ILogger<TransactionConsumer> logger,
+                               IServiceProvider serviceProvider,
+                               IOptions<ServerSettingsRabbitMQ> rabbitServerSettings) 
+                               : base(configuration["TransactionSettings:QueueName"] ?? string.Empty, rabbitServerSettings)
     {
+        _logger = logger;
+        _serviceProvider = serviceProvider;
     }
 
-    protected override Task ConsumeMessage(string message)
+    protected override async Task ConsumeMessage(string message)
     {
-        return Task.CompletedTask ;
+        if (string.IsNullOrEmpty(message))
+        {
+            _logger.LogError($"{nameof(TransactionConsumer)} - Mensagem recebia vazia ou nula");
+            return;
+        }
+
+        var transaction = JsonSerializer.Deserialize<Transaction>(message);
+
+        if (transaction == null)
+        {
+            _logger.LogError($"{nameof(TransactionConsumer)} - Falha ao deserializar a transaction message");
+            return;
+        }
+
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var customerService = scope.ServiceProvider.GetRequiredService<ICustomerService>();
+            await customerService.UpdateLimit(transaction.CustomerId, transaction.Amount);
+        }
     }
 }
